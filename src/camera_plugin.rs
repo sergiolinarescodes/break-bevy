826bead1aaae82d4631ffb8adc700cd284551c89
Sergio Linares Peralta <sergiolinaresperalta@gmail.com>
2023-11-29 19:59:30 +0100

    added default camera

---
+++ src/camera_plugin.rs
@@ -0,0 +1,27 @@
+use bevy::prelude::*;
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+mod camera_plugin;
 use bevy::prelude::*;
 
+use camera_plugin::CameraPlugin;
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(CameraPlugin)
+        .run();
 }
